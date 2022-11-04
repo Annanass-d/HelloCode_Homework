571a7d7a05808021fe56172e399d6f21c5c47b45
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-04 12:45:53 +0300

    add Task1(10)

---
+++ Seminar2/Task1(10)/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+var random = new Random();
+int num = random.Next(100,1000);
+System.Console.WriteLine(num);
+
+int numFirstSecond = num / 10;
+int numSecond = numFirstSecond % 10;
+
+System.Console.WriteLine(numSecond);
