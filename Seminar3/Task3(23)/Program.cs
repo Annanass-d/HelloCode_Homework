a0cbc495027ae451d32902d990eb15be7ead9ddd
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-07 03:35:39 +0300

    add Task3(23)

---
+++ Seminar3/Task3(23)/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+System.Console.WriteLine("Введите число:");
+double num = Convert.ToDouble(Console.ReadLine());
+
+if (num <= 0){
+    System.Console.WriteLine("Введен ноль или отрицательное число, введите другое число");
+}
+
+for (double i = 1; i <= num; i++)
+{
+    double result = Math.Pow(i, 3);
+    System.Console.Write(result + "  ");
+}
