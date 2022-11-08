3137a69d949f4962b62a437472e90b33e30b72a4
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-08 18:49:22 +0300

    add Task2(27)

---
+++ Seminar4/Task2(27)/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SetNumber()
+{
+    System.Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int SumFigures(int number)
+{
+    int SumFig = 0;
+    int count = 0; // для перебора цифр в числе
+
+    for (int countNum = number; countNum >0; countNum/=10)
+    {
+        count = countNum %10;
+        SumFig += count; 
+    }
+    return SumFig;
+}
+
+
+int num = SetNumber();
+int result = SumFigures(num);
+System.Console.WriteLine(result);
