9c52c4327e517a01b30346eb37cdcfa63b6123a3
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-08 18:29:58 +0300

    add Task1(25)

---
+++ Seminar4/Task1(25)/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int SetNumber(string str)
+{
+    System.Console.WriteLine($"Введите число {str}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int NumAPowB (int numA, int numB)
+{
+    int result = numA;
+    for (int i = 1; i < numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+
+int numberA = SetNumber("A");
+int numberB = SetNumber("B");
+int res = NumAPowB(numberA,numberB);
+System.Console.WriteLine(res);
