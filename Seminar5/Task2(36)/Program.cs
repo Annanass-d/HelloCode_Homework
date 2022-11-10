56851c3b92949ec5fe67fcbf24f0e95f29924e10
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-10 18:45:59 +0300

    add \Task2(36)"

---
+++ Seminar5/Task2(36)/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetRandomArray(int size, int begin, int last)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(begin, last + 1);
+    }
+    return array;
+}
+
+int SumUnevenPos(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            count += array[i];
+        }
+    }
+    return count;
+}
+
+
+int[] arr = GetRandomArray(6, -100, 101);
+System.Console.WriteLine(String.Join(", ", arr));
+
+int result = SumUnevenPos(arr);
+System.Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {result}");
